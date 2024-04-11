9d5762cc40aa802dab8d35267580da05e644c37e
Ferlando <contact@ferlando.me>
2024-04-11 19:38:09 +0200

    update strict

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,7 +25,7 @@ def c_is_fun(text):
     return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python')
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_is_cool(text='is cool'):
     """Displays 'Python' followed by the value of the text variable"""
